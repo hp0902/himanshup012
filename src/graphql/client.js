108a373f295ac10dba7614d350adc376c84d8d30
hp0902 <hp09_02@yahoo.com>
2025-06-18 12:05:13 -0400

    Update client.js

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,3 +1,15 @@
 import { GraphQLClient } from 'graphql-request';
-
+query {
+  profilebanner {
+    backgroundImage {
+      url
+    }
+    headline
+    resumeLink {
+      url
+    }
+    linkedinLink
+    profileSummary
+  }
+}
 const endpoint
