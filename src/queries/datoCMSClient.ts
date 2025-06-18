584e245f77da11fb3287f98f316c371e51b661d4
hp0902 <hp09_02@yahoo.com>
2025-06-18 11:07:34 -0400

    Update datoCMSClient.ts

--- src/queries/datoCMSClient.ts
+++ src/queries/datoCMSClient.ts
@@ -3,12 +3,11 @@ import { getDatoCmsToken } from './getDatoCmsToken';
 
 const DATO_CMS_ENDPOINT = 'https://graphql.datocms.com/';
 const DATO_CMS_API_TOKEN = getDatoCmsToken();
-console.log(DATO_CMS_API_TOKEN)
+
 const datoCMSClient = new GraphQLClient(DATO_CMS_ENDPOINT, {
   headers: {
     Authorization: `Bearer ${DATO_CMS_API_TOKEN}`,
   },
-  
 });
 
 export default datoCMSClient;
