e396a27f8bfc9ac83b0c54a4ba30f818f9ca0cf2
hp0902 <hp09_02@yahoo.com>
2025-06-17 23:09:03 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -36,7 +36,7 @@ export interface Project {
 export interface Certification {
   title: string;
   issuer: string;
-  issuedDate: string;
+  issueddate: string;
   link: string;
   iconName: string;
 }
