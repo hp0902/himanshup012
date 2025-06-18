42cf08ea032eacfb9bcfa7a1b99cd983c2a22611
hp0902 <hp09_02@yahoo.com>
2025-06-18 12:00:27 -0400

    Update datoCMSClient.ts

--- src/queries/datoCMSClient.ts
+++ src/queries/datoCMSClient.ts
@@ -1,22 +1,15 @@
-// getDatoCmsToken.ts
-export const getDatoCmsToken = (): string => {
-  const environment = process.env.REACT_APP_ENV || 'development';  // Default to 'development' if not set
+// datoCMSClient.ts
 
-  let token = '';
-  switch (environment) {
-    case 'production':
-      token = process.env.REACT_APP_DATOCMS_PROD_TOKEN ?? '';
-      break;
-    case 'staging':
-      token = process.env.REACT_APP_DATOCMS_STAGING_TOKEN ?? '';
-      break;
-    case 'development':
-      token = process.env.REACT_APP_DATOCMS_DEV_TOKEN ?? '';
-      break;
-    default:
-      throw new Error(`No DatoCMS token configured for environment: ${environment}`);
-  }
+import { GraphQLClient } from 'graphql-request';
+import { getDatoCmsToken } from './getDatoCmsToken';
 
-  console.log('Using token for', environment, ':', token);  // Log token for debugging
-  return token;
-};
+const DATO_CMS_ENDPOINT = 'https://graphql.datocms.com/';
+const DATO_CMS_API_TOKEN = getDatoCmsToken();
+
+const datoCMSClient = new GraphQLClient(DATO_CMS_ENDPOINT, {
+  headers: {
+    Authorization: `Bearer ${DATO_CMS_API_TOKEN}`,
+  },
+});
+
+export default datoCMSClient; 
