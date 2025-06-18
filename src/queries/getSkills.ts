acfb45ac5a80675c5808dc8e35756b702a75b2b3
hp0902 <hp09_02@yahoo.com>
2025-06-17 22:50:33 -0400

    Update getSkills.ts

--- src/queries/getSkills.ts
+++ src/queries/getSkills.ts
@@ -1,7 +1,7 @@
 // queries/getTimeline.ts
 import datoCMSClient from './datoCMSClient';
 import { Skill } from '../types';
-import { getDatoCmsToken } from './getDatoCmsToken';
+
 const GET_SKILLS = `
 {
   allSkills(orderBy: category_ASC) {
@@ -14,8 +14,6 @@ const GET_SKILLS = `
 `;
 
 export async function getSkills(): Promise<Skill[]> {
-  const DATO_CMS_API_TOKEN = getDatoCmsToken();
-  console.log(DATO_CMS_API_TOKEN);
   const data = await datoCMSClient.request<{ allSkills: Skill[] }>(GET_SKILLS);
   return data.allSkills;
 }
