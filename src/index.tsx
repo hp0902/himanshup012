e6b0dc2ec055f4c686899fbbdd49a17d0682afe8
hp0902 <hp09_02@yahoo.com>
2025-06-19 13:02:50 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import App from './App';
-import './index.css'; // Optional: Include your global styles here if necessary
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
-/*root.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <Router>
     <App />
   </Router>
-);*/
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
