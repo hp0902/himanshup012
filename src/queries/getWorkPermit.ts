8a566cbcff9f60f7fa2449a4e972a05b69bdac81
hp0902 <hp09_02@yahoo.com>
2025-06-18 21:37:45 -0400

    Update getWorkPermit.ts

--- src/queries/getWorkPermit.ts
+++ src/queries/getWorkPermit.ts
@@ -1,7 +1,12 @@
 // queries/getWorkPermit.ts
 import datoCMSClient from './datoCMSClient';
 import { WorkPermit } from '../types';
-
+ const response = await fetch('https://graphql.datocms.com/', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer YOUR_API_KEY`, // Use your actual API key here
+      'Content-Type': 'application/json',
+    },
 const GET_WORK_PERMIT = `
   query MyQuery {
   workPermit {
