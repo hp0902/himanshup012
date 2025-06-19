930dd92f566ca30ef2c01bf05e97e8568f0e9368
hp0902 <hp09_02@yahoo.com>
2025-06-19 12:25:55 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import App from './App';
 import './index.css'; // Optional: Include your global styles here if necessary
 
 ReactDOM.render(<App />, document.getElementById('root'));
