dbb011268f925e852cdfdd56456a28d770118557
hp0902 <hp09_02@yahoo.com>
2025-06-18 21:29:13 -0400

    Update WorkPermit.tsx

--- src/pages/WorkPermit.tsx
+++ src/pages/WorkPermit.tsx
@@ -1,28 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './WorkPermit.css';
-import { getWorkPermit } from '../queries/getWorkPermit';
-import { WorkPermit as IWorkPermit } from '../types';
+
 const WorkPermit: React.FC = () => {
-
-  const [workPermitData, setWorkPermitData] = useState<IWorkPermit | null>(null);
-  useEffect(() => {
-    async function fetchWorkPermitData() {
-      const data = await getWorkPermit();
-      setWorkPermitData(data);
-    }
-    fetchWorkPermitData();
-  }, []);
-
-  if (!workPermitData) return <div>Loading...</div>;
-
   return (
     <div className="work-permit-container">
       <div className="work-permit-card">
-        <h2 className="work-permit-headline">🎓 Us Citizen</h2>
+        <h2 className="work-permit-headline">🎓 US Citizen</h2>
         <p className="work-permit-summary">
           I'm a US Citizen 🌟
         </p>
-        <p className="additional-info">{workPermitData.additionalInfo}</p>
       </div>
     </div>
   );
