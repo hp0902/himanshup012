27699534f6ef0386f375256a39e8ab1fdcbc95b3
hp0902 <hp09_02@yahoo.com>
2025-06-18 20:42:07 -0400

    Update getProjects.ts

--- src/queries/getProjects.ts
+++ src/queries/getProjects.ts
@@ -2,18 +2,17 @@
 import datoCMSClient from './datoCMSClient';
 import { Project } from '../types';
 
-const GET_PROJECTS = `
-  query {
-    allProjects(orderBy: title_ASC) {
-      title
-      description
-      techUsed
-      image {
-        url
-      }
-    }
-  }
-`;
+const data = [
+  {
+    title: "Project 1",
+    Situation: "The situation here...",
+    Task: "The task to solve...",
+    Action: "The action taken...",
+    Result: "The result achieved...",
+    techused: "ReactJS, NodeJS"
+  },
+  // more projects
+];
 
 export async function getProjects(): Promise<Project[]> {
   const data = await datoCMSClient.request<{ allProjects: Project[] }>(GET_PROJECTS);
